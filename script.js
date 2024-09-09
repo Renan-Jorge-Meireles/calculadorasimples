0329e5a462f44fa10d053bfa60fef131d7ec24d6
Renan-Jorge-Meireles <159266744+Renan-Jorge-Meireles@users.noreply.github.com>
2024-09-09 09:27:48 -0300

    09-09
    coisas

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-let a = cad
-let b = cad
+function calculo(sla)
+let a = parseFloat(document.getElementById('zeroum').value);
+let b = parseFloat(document.getElementById('zerodois').value);
+let resultado = 0
+
+if (isNaN(a) || isNaN(b)) {
+    alert('otario');
+}else{
+
+
+if(sla == '+'){
+    resultado = a + b;
+}
+ else if(sla == '-'){
+    resultado = a - b;
+}
+ else if(sla == '*'){
+    resultado = a * b;
+ }
+ else if(sla == '/'){
+    resultado = a / b;
+ }
+}
+document.getElementById('resultado').textContent = "resultado" + resultado
